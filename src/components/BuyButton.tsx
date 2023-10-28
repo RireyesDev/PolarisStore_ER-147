bc984dc67b220a268064c7bb1f22b71f6c34c287
Eduardo <eduardorireyes2024@gmail.com>
2023-10-28 02:30:02 -0600

    [Update] Cree por segunda vez el componente Buy Button

---
+++ src/components/BuyButton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import { PrimaryBTN } from "./PrimaryButton";
+
+
+const BuyBTN = styled(PrimaryBTN)`
+    background-color: var(--primary-color_triad);
+
+    &:hover{
+        background-color: var(--primary-color_triad_hover);
+    }
+
+    &:active{
+        background-color: var(--primary-color_triad_active);
+    }
+
+`;
+
+const BuyButton = (props: any) => {
+    return(
+        <BuyBTN>{props.children}</BuyBTN>
+    );
+}
+
+export { BuyButton }
