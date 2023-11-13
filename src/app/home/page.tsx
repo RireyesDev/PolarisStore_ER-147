ee3c251da08efc6d7ed42fbcda5a27a9e3e5eba2
Eduardo <eduardorireyes2024@gmail.com>
2023-11-12 19:32:28 -0600

    [Add/Update] Cree la tabla de cards para los productos

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 import { NavBar } from "@/components/NavBar";
 import { Productcard } from "@/components/ProductCard";
+import { TableProductCards } from "@/components/TableProductCards";
 
 
 export default function Welcome() {
     return (
         <>
             <NavBar></NavBar>
-            <Productcard></Productcard>
+            <TableProductCards></TableProductCards>
         </>
     );
 }
