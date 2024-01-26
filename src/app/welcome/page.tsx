f64cae4789781e2252d48a7e6a44031e024ce542
EduardoRireyes24 <eduardorireyes2024@gmail.com>
2024-01-25 20:56:50 -0600

    [Responsive Update] Volvi responsivo la pagina de Welcome

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import { CardLogIn } from "@/components/Card_LogIn";
 import Image from "next/image";
 import PolarisLight from "/public/Polaris_Light.svg";
-import Product1 from "/public/product_microfone1.jpg";
 import Product2 from "/public/product_microfone2.jpg";
 import { useRouter } from "next/navigation";
 
@@ -13,67 +12,102 @@ const ContainerPage = styled.div`
     width: 100%;
     min-height: 100vh;
     display: grid;
-    grid-template-columns: repeat(2, 50%); 
+    grid-template-columns: 100%; 
     align-items: center;
     justify-content: center;
+
+    @media (max-width: 768px){
+        min-height: 110vh;     
+    }
 `;
 
-const PrimaryBackground = styled.section`
+const Background = styled.section`
     width: 100%;
-    height: 100%;
+    min-height: 100vh;
     grid-column: 1 / 2;
-    background-image: url(${Product1.src});
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;    
-    display: flex;
-    flex-direction: column;
-    justify-content: start;
-    padding: 4%;
-`;
-
-const PolarisLogo = styled(Image)`
-    width: 300px;
-    height: 200px;
-    align-self: left;
-`;
-
-const SecondBackground = styled(PrimaryBackground)`
-    grid-column: 2 / 3;
     background-image: url(${Product2.src});
     display: grid;
     place-content: center;
     padding: 0;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;        
+    grid-template-columns: 50% 50%;
+    flex-direction: column;
+    justify-content: start;
+    padding: 1% 8%;
+
+    @media (max-width: 768px){
+        grid-template-columns: 100%;
+        grid-template-rows: 10% 20% 70%;
+        min-height: 110vh;
+        grid-column: 1 / 2;
+        grid-row: 2 / 3;
+    }
 `;
 
+const PolarisLogo = styled(Image)`
+    width: 300px;
+    height: 200px;   
+    align-self: left;
 
+    @media (max-width: 768px){     
+        width: 200px;
+        height: 100px;   
+        grid-column: 1 / 2;
+        grid-row: 1 / 2;
+        justify-self: center;
+        align-self: center;
+    }
+`;
 
 const WelcomeText = styled.h1`
     width: 100%;
     color: var(--light-color);
-    font-size: 38px;
+    font-size: 34px;
     letter-spacing: 2px;
-    align-self: center;    
-    margin-top: 32%;
+    justify-self: center;
+    align-self: center; 
+    grid-column: 1 / 2;
 
+    @media (max-width: 768px){        
+        grid-column: 1 / 2;
+        grid-row: 2 / 3;
+        justify-self: center;
+        align-self: center;
+        font-size: 28px;
+        text-align: center;
+    }
 
+`;
+
+const CardLogInContainer = styled.div`
+    justify-self: right;
+    align-self: center;
+    grid-column: 2 / 3;
+    grid-row: 1 / 3;
+
+    @media (max-width: 768px){        
+        grid-column: 1 / 2;
+        grid-row: 3 / 4;
+        justify-self: center;
+        align-self: center;
+    }
 `;
 
 export default function Welcome() {
 
-    const router = useRouter();
     const StoreDescription = "Obten la mejor experiencia y compra los mejores productos.";
 
     return (
         <ContainerPage>
-            <PrimaryBackground>
+            <Background>                
                 <PolarisLogo src={PolarisLight} alt="Polaris Logo"></PolarisLogo>
                 <WelcomeText>{StoreDescription}</WelcomeText>
-            </PrimaryBackground>
-
-            <SecondBackground>                
-                <CardLogIn/>                
-            </SecondBackground>
+                <CardLogInContainer>
+                    <CardLogIn></CardLogIn>
+                </CardLogInContainer>
+            </Background>
         </ContainerPage>
     );
 }
