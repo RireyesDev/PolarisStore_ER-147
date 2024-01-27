b6da8e1e4e2d91eb5d347914b726fc5bc7234de7
EduardoRireyes24 <eduardorireyes2024@gmail.com>
2024-01-26 19:10:01 -0600

    [Responsive Update] Volvi responsiva la pagina para ver Mi Cuenta

--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -2,7 +2,6 @@
 import { useContext } from "react";
 import styled from "styled-components";
 import { PrimaryButton } from "@/components/PrimaryButton"
-import { TitlePage } from "@/components/TitlePage";
 import { Navigator } from "@/containers/Navigator";
 import { ImageProfile } from "@/components/ImageProfile";
 import { useState } from "react";
@@ -18,6 +17,11 @@ const MainContainer = styled.main`
     min-height: 92vh;
     background-color: white;
     display: grid;    
+
+    @media (max-width: 768px) {
+        min-height: 100vh;
+        grid-template-columns: 1fr;
+    }
 `;
 
 const InfoContainer = styled.section`
@@ -27,6 +31,63 @@ const InfoContainer = styled.section`
     grid-template-columns: 20% 40% 40%;
     grid-template-rows: repeat(3, 33.33%);      
     margin-top: 40px;
+
+    @media (max-width: 768px) {
+        margin-top: 0px;
+        height: 210px;
+        grid-template-columns: 40% 60%;
+        grid-template-rows: repeat(3, 20%) 40%;
+    }
+`;
+
+
+const ImageProfileContainer = styled.div`
+    grid-column: 1 / 2;
+    grid-row: 2 / 3;
+    display: grid;
+    place-content: center;
+
+    @media (max-width: 768px) {
+        grid-column: 1 / 2;
+        grid-row: 1 / 4;
+    }
+`;
+
+
+const NameProfile = styled.h4`
+    width: 100%;
+    font-size: 18px;
+    grid-column: 2 / 3;
+    grid-row: 1 / 2;
+    display: grid;
+    place-content: center start;
+    font-family: 'Nunito', sans-serif;
+
+    @media (max-width: 768px) {
+        font-size: 16px;
+        grid-column: 2 / 3;
+        grid-row: 1 / 2;
+    }
+`;
+
+const EmailProfile = styled(NameProfile)`
+    font-size: 16px;
+    grid-column: 2 / 3;
+    grid-row: 2 / 3;
+
+    @media (max-width: 768px) {
+        font-size: 14px;
+    }
+`;
+
+const CountryProfile = styled(NameProfile)`
+    font-size: 16px;
+    grid-column: 2 / 3;
+    grid-row: 3 / 4;
+
+    @media (max-width: 768px) {
+        font-size: 14px;
+    }
 `;
 
 const GridContainer = styled.div`
@@ -34,9 +95,14 @@ const GridContainer = styled.div`
     display: grid;
     grid-template-columns: 60% 40%;
     grid-template-rows: 1fr 2fr 2fr;
-    min-height: 1100px;
+    min-height: 1000px;
     padding-bottom: 40px;
     justify-self: center;
+
+    @media (max-width: 768px) {
+        grid-template-columns: 1fr;
+        grid-template-rows: 20% 40% 40%;
+    }
 `;
 
 const DivContainer = styled.div`
@@ -47,16 +113,28 @@ const DivContainer = styled.div`
     display: grid; 
     align-content: center;
     justify-content: right; 
+
+    @media (max-width: 768px) {
+        grid-column: 2 / 3;
+        grid-row: 4 / 5;
+        align-self: start;
+    }
 `;
 
 
 const CardSection = styled.div`
     width: 100%;
-    height: 100%;
+    height: 80%;
     display: grid;
     grid-template-columns: 20% 40% 40%;
     grid-template-rows: repeat(3, 33.33%);
     border-radius: 10px;
+    
+    @media (max-width: 768px) {
+        margin-top: 40px;
+        grid-template-rows: 20% 50% 10% 20%;
+        grid-template-columns: 40% 60%;
+    }
 `;
 
 const CardSection2 = styled(CardSection)`
@@ -76,6 +154,12 @@ const Card = styled.div`
   padding: 10px;
   grid-column: 1 / 4;
   grid-row: 2 / 4;
+
+  @media (max-width: 768px) {
+    height: 140px;
+    align-self: center;
+    grid-row: 2 / 3;
+  }
 `;
 
 const TitleAccount = styled.h3`
@@ -89,36 +173,6 @@ const TitleAccount = styled.h3`
 `;
 
 
-const ImageProfileContainer = styled.div`
-    grid-column: 1 / 2;
-    grid-row: 1 / 4;
-    display: grid;
-    place-content: center;
-`;
-
-const NameProfile = styled.h4`
-    width: 100%;
-    font-size: 18px;
-    grid-column: 2 / 3;
-    grid-row: 1 / 2;
-    display: grid;
-    place-content: center start;
-    font-family: 'Nunito', sans-serif;
-`;
-
-const EmailProfile = styled(NameProfile)`
-    font-size: 16px;
-    grid-column: 2 / 3;
-    grid-row: 2 / 3;
-`;
-
-const CountryProfile = styled(NameProfile)`
-    font-size: 16px;
-    grid-column: 2 / 3;
-    grid-row: 3 / 4;
-`;
-
-
 const CardsEditContainer = styled.section`
     position: absolute;
     width: 100%;
@@ -127,6 +181,31 @@ const CardsEditContainer = styled.section`
     place-content: center;
     background-color: #1414141a;
     margin: 0 auto;
+`;
+
+const TitlePage = styled.h2`
+    width: 100%;
+    height: 80px;
+    display: grid;
+    place-content: center;
+    font-weight: bold;
+    margin-top: 100px;
+    margin-bottom: 40px; 
+    letter-spacing: 3px;
+    font-size: 32px;
+    font-family: 'Nunito', sans-serif;
+
+    @media (max-width: 768px) {
+        height: 38px;
+        font-size: 28px;
+        grid-column: 1 / 2;
+        grid-row: 1 / 2;
+        justify-self: center;
+        align-self: center;
+        margin-top: 80px;
+        margin-bottom: 40px; 
+        text-align: center;
+    }
 `;
 
 
